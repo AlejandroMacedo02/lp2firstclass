51cb2df823fba855ce99b1f9894675feb5599ed9
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-15 16:08:16 -0500

    creating packages

---
+++ src/main/java/upeu/edu/pe/lp2classproyect/Lp2classproyectApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.lp2classproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2classproyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2classproyectApplication.class, args);
+	}
+
+}
