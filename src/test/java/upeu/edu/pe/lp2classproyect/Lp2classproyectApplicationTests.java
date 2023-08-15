6ee290a669a7faf3b65635718d853af16646dad4
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-15 16:13:18 -0500

    creating packages

---
+++ src/test/java/upeu/edu/pe/lp2classproyect/Lp2classproyectApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.lp2classproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2classproyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
