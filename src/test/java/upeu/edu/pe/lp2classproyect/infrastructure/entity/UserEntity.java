39d84e8b01588d304cf6117e759d2987411ea9c3
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-15 16:28:10 -0500

    get and set de user(entity)

--- src/test/java/upeu/edu/pe/lp2classproyect/infrastructure/entity/UserEntity.java
+++ src/test/java/upeu/edu/pe/lp2classproyect/infrastructure/entity/UserEntity.java
@@ -4,10 +4,104 @@
  */
 package upeu.edu.pe.lp2classproyect.infrastructure.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
 /**
  *
  * @author alejandromacedop
  */
+@Entity
+@Table (name = "users")
 public class UserEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+    private String cellphone;
+    private String password;
+    private LocalDateTime dateCreated;
+
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+    
+    
     
 }
