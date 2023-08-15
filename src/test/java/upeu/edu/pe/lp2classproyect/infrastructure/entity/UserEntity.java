ee23adce874f04b66cf74c1361b8df08aee545f2
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-15 16:17:23 -0500

    creating entity (user y producto)

---
+++ src/test/java/upeu/edu/pe/lp2classproyect/infrastructure/entity/UserEntity.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package upeu.edu.pe.lp2classproyect.infrastructure.entity;
+
+/**
+ *
+ * @author alejandromacedop
+ */
+public class UserEntity {
+    
+}
