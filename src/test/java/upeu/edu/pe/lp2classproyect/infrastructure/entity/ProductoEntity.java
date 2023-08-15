ecb919a284b67802882bc76601a8df9de2ea85cc
AlejandroMacedo02 <106896409+AlejandroMacedo02@users.noreply.github.com>
2023-08-15 16:34:49 -0500

    getter and setter completados (ProductoEntity y UserEntiy)

--- src/test/java/upeu/edu/pe/lp2classproyect/infrastructure/entity/ProductoEntity.java
+++ src/test/java/upeu/edu/pe/lp2classproyect/infrastructure/entity/ProductoEntity.java
@@ -34,7 +34,98 @@ public class ProductoEntity {
     private LocalDateTime dateUpdate;
     
     @ManyToOne
-    private UserEntity UserEntity;   
+    private UserEntity UserEntity;  
+
+    public ProductoEntity() {
+    }
+
+    public ProductoEntity(Integer id, String code, String name, String description, String image, BigDecimal price, LocalDateTime dateCreated, LocalDateTime dateUpdate, UserEntity UserEntity) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.dateCreated = dateCreated;
+        this.dateUpdate = dateUpdate;
+        this.UserEntity = UserEntity;
+    }
+    
+    
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getDateUpdate() {
+        return dateUpdate;
+    }
+
+    public void setDateUpdate(LocalDateTime dateUpdate) {
+        this.dateUpdate = dateUpdate;
+    }
+
+    public UserEntity getUserEntity() {
+        return UserEntity;
+    }
+
+    public void setUserEntity(UserEntity UserEntity) {
+        this.UserEntity = UserEntity;
+    }
+    
     
 }
 
